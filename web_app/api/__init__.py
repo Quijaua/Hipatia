c3a88442a9a729cd54b0ade4ba8a616112d5c795
motive-mobi <motive.mobi@gmail.com>
2025-03-26 07:52:25 -0300

    [fix] adicionado .env para credenciais de acesso ao banco de dados

--- web_app/api/__init__.py
+++ web_app/api/__init__.py
@@ -3,13 +3,17 @@ import requests
 
 from flask import Flask, request
 from .database.models import db, User, Book, BookStatus, Loan
+from dotenv import load_dotenv
 
 
 def create_app(test_config=None):
+    load_dotenv()
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3er45ty6@172.17.0.2:5432/hipatia'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:3er45ty6@172.17.0.2:5432/hipatia'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
